65ed3d3c572875465d4287824ac5e01874f0624b
Maxim Kalaev <maximk@il.ibm.com>
2011-01-18 13:28:30 +0200

    emacs: supporting opening file at the specified line

--- scripts/e.sh
+++ scripts/e.sh
@@ -4,7 +4,13 @@ if ! emacsclient -e t >/dev/null 2>&1
 then
     exec emacs "$@" &
 else
-#echo emacsclient -n "$@"
-for f in "$@"; do emacsclient -n $f; done
-#exec emacsclient -n "$@"
+for f in "$@"; do
+    if [[ $f =~ "(.*):([0-9]+)" ]]
+    then
+        emacsclient -n +${BASH_REMATCH[2]} ${BASH_REMATCH[1]}
+    else
+        emacsclient -n $f
+    fi
+# emacsclient -ne '(gtags-goto-tag "invoke_operation" "")'
+done
 fi
