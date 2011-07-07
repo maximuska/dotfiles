a843274e24f256cd34fc4c52a583441fd9a44e2a
Maxim Kalaev <maximus.ka@gmail.com>
2011-07-07 10:40:35 +0300

    Enhance tags-build to search for cpp, xml and py files as well

--- scripts/tags_build.sh
+++ scripts/tags_build.sh
@@ -1,5 +1,5 @@
 echo Running...
-find . -name "*.c" -or -name "*.h" | grep -v .xmake.build > .files
+find -name "*.c" -or -name "*.cc" -or -name "*.h" -or -name "*.xml" -or -name "*.py" | grep -v .xmake.build > .files
 echo Total files: `wc -l .files`
 cat .files | pv -l | etags -L -
 cat .files | pv -l | ctags -L -
