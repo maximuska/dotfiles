ee283369be7fad6b6767b04d2a8ce8c078e006e7
Maxim Kalaev <maximus.ka@gmail.com>
2017-01-31 16:00:50 +0200

    Minor scripts updates

--- scripts/tags_build.sh
+++ scripts/tags_build.sh
@@ -1,9 +1,16 @@
+#!/bin/bash -e
+
 echo Running...
-find -name "*.c" -or -name "*.cc" -or -name "*.C" -or -name "*.h" -or -name "*.xh" -or -name "*.xml" -or -name "*.py" | grep -v .xmake.build | grep -v "_test.c" > .files
+find -not \( -path \*/.xmake.build* -prune \) -not \( -path ./kernel/tree\* -prune \) -not \( -path ./new_outputs -prune \) \
+  -name "*.c" -or -name "*.cc" -or -name "*.C" -or -name "*.h" -or -name "*.xh" -or -name "*.xml" -or -name "*.py" -or -name "*.pyx" > .files
 echo Total files: `wc -l .files`
-gtags -I -f .files&
-etags -L .files&
-ctags -L .files&
+#exit 1
+#gtags -I -f .files&
+gtags -f .files&
+#etags -L .files&
+ctags -L .files --c++-kinds=+p --fields=+iaS --extra=+q --extra=+f --langmap=c:+.xh -h+.xh -IUNINSTRUMENTED&
+#cscope -b -q -v -i.files&
 #cat .files | pv -l | cscope -b -i -
 echo Waiting for tags to be prepared..
 wait
+rm -f .files
